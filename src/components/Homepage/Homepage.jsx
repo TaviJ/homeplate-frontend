388dd808f70c552c6cb392ea734dcf2e28de5a05
Gabriela Araujo <gabyara237@gmail.com>
2026-01-22 10:41:15 -0800

    feat: add links to recipe details from homepage

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,9 +1,19 @@
+import { Link } from "react-router";
+
 const RecipeList = ({recipes}) =>{
     return (
         <main>
             {
-                recipes.map((recipes)=>(
-                    <p key={recipes._id}>{recipes.title}</p>
+                recipes.map((recipe)=>(
+                    <Link key={recipe._id} to = {`/recipes/${recipe._id}`}>
+                        <article>
+                            <header>
+                                <h2>{recipe.title}</h2>
+                                <p>{`${recipe.author.username} posted on ${new Date(recipe.createdAt).toLocaleDateString()}`}</p>
+                            </header>
+                            <p>{recipe.description}</p>
+                        </article>
+                    </Link>
                 ))
             }
         </main>
