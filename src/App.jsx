9592821efab34663b12cc16e0f1c004ea961f996
Gabriela Araujo <gabyara237@gmail.com>
2026-01-22 19:50:21 -0800

    feat: handle recipe creation and state update in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+import { Routes, Route, useNavigate } from 'react-router';
+import { useEffect, useState } from 'react';
+
 import * as recipeService from './services/recipeService'
 
 import Homepage from './components/Homepage/Homepage';
 import RecipeDetails from './components/RecipeDetails/RecipeDetails';
-
+import RecipeForm from './components/RecipeForm/RecipeForm';
 import './App.css'
 
-import { Route, Routes } from 'react-router';
-import { useEffect, useState } from 'react';
 
 const App = () => {
 
+  const navigate = useNavigate();
   const [recipes, setRecipes] = useState([])
 
   useEffect(()=>{
@@ -20,11 +22,45 @@ const App = () => {
     fetchAllRecipes()
   },[])
 
+  const handleAddRecipe = async (recipeFormData) => {
+    const payload = {
+      title: recipeFormData.title,
+      description: recipeFormData.description,
+      imageUrl: recipeFormData.imageUrl,
+      difficultyLevel: recipeFormData.difficultyLevel,
+      typeRecipe: recipeFormData.typeRecipe,
+
+      prepTime: Number(recipeFormData.prepTime),
+      servings: Number(recipeFormData.servings),
+
+      ingredients: recipeFormData.ingredients.map((ingredient) => ({
+        nameIngredient: ingredient.nameIngredient,
+        unitQuantity: Number(ingredient.unitQuantity),
+      })),
+
+      steps: recipeFormData.steps.map((step) => ({
+        stepNumber: Number(step.stepNumber),
+        stepTitle: step.stepTitle,
+        stepDescription: step.stepDescription,
+      })),
+
+      tags: recipeFormData.tags
+        .split(",")
+        .map((tag) => tag.trim())
+        .filter(Boolean),
+    };
+    const newRecipe= await recipeService.create(payload);
+    setRecipes([newRecipe, ...recipes])
+    navigate("/recipes");
+  };
+
   return(
     <>
       <Routes>
         <Route path='/recipes' element={<Homepage recipes={recipes}/>}/>
+        <Route path='/recipes/new' element={<RecipeForm handleAddRecipe={handleAddRecipe} />}/>
         <Route path='/recipes/:recipeId' element={<RecipeDetails/>}/>
+        
       </Routes>
     </>
   );
