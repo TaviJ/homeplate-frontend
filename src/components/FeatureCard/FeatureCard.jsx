ffc1c66940fb23117cf78daf65c862efd9e42be9
User Name <user@email.com>
2026-01-26 12:12:40 -0700

    feat: create FeatureCard component for homepage

---
+++ src/components/FeatureCard/FeatureCard.jsx
@@ -0,0 +1,12 @@
+const FeatureCard = ({image,title,description})=> {
+  
+  return( 
+    <div className="feature-card">
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+       <p>{description}</p>
+    </div>
+  )
+
+  }
+  export default FeatureCard
