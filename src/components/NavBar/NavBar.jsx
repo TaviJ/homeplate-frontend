7d2a77b208de09b4aef59a1a462faf3000e25ed2
Gabriela Araujo <gabyara237@gmail.com>
2026-01-28 21:47:28 -0800

    style: implement sidebar navbar with active links and icons

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,18 @@
+import { Link, NavLink } from 'react-router';
 import { useContext } from 'react';
-import { Link } from 'react-router';
 import { UserContext } from '../../contexts/UserContext';
+import './navbar.css'
+import HomeIcon from '../icons/HomeIcon';
+import Profile from '../icons/Profile';
+import Food from '../icons/Food';
+import SignOut from '../icons/SignOut';
+import Follow from '../icons/Follow';
+import Add from '../icons/Add';
 
 const NavBar = () => {
-  const { user, setUser } = useContext(UserContext);
+
+
+  const { setUser } = useContext(UserContext);
 
   const handleSignOut = () => {
     localStorage.removeItem('token');
@@ -11,26 +20,47 @@ const NavBar = () => {
   };
 
   return (
-    <nav>
-      {user ? (
-        <ul>
-          <li>Welcome, {user.username}</li>
-          <li><Link to="/home">Home</Link></li>
-          <li><Link to="/profile">Profile</Link></li>
-          <li><Link to="/add-recipe">Add New Recipe</Link></li>
-          <li><Link to="/my-recipes">My Recipes</Link></li>
-          <li><Link to="/follow">Followers & Following</Link></li>
-          <li>
-            <Link to="/" onClick={handleSignOut}>Sign Out</Link>
+    <nav className='sidebar'>
+  
+        <ul className='ul-nav'>
+          <li className="nav-logo"><h2 className="logo">HomePlate</h2></li>
+          <li className='nav-list'>
+            <NavLink className={({ isActive }) => isActive ? "nav-link active" : "nav-link" } to="/">
+              <HomeIcon className="icon" /> Home
+            </NavLink>
           </li>
+
+          <li className='nav-list'>
+            <NavLink className={({ isActive }) => isActive ? "nav-link active" : "nav-link" } to="/profile">
+              <Profile className="icon" /> Profile
+            </NavLink>
+          </li>
+
+          <li className='nav-list'>
+            <NavLink className={({ isActive }) => isActive ? "nav-link active" : "nav-link" } to="/recipes/new">
+            <Add className="icon"/>  Add New Recipe
+            </NavLink>
+          </li>
+
+          <li className='nav-list'>
+            <NavLink className={({ isActive }) => isActive ? "nav-link active" : "nav-link" } to="/my-recipes">
+            <Food className="icon"/>  My Recipes
+            </NavLink>
+          </li>
+
+          <li className='nav-list'>
+            <NavLink className={({ isActive }) => isActive ? "nav-link active" : "nav-link" } to="/follow">
+            <Follow className="icon"/>  Followers & Following
+            </NavLink>
+          </li>
+
+          <li className='nav-list'>
+            <Link className="nav-link"  to="/" onClick={handleSignOut}>
+              <SignOut className="icon"/> Sign Out
+            </Link>
+          </li>
+
         </ul>
-      ) : (
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/sign-in">Sign In</Link></li>
-          <li><Link to="/sign-up">Sign Up</Link></li>
-        </ul>
-      )}
     </nav>
   );
 };
